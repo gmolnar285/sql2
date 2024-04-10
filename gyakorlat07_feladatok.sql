492b17d7489df7be337bf18b1c439b1e6dc6901f
gmolnar285 <40260228+gmolnar285@users.noreply.github.com>
2024-04-10 10:24:45 +0200

    Add files via upload

---
+++ gyakorlat07_feladatok.sql
@@ -0,0 +1,93 @@
+{
+    "metadata": {
+        "kernelspec": {
+            "name": "SQL",
+            "display_name": "SQL",
+            "language": "sql"
+        },
+        "language_info": {
+            "name": "sql",
+            "version": ""
+        }
+    },
+    "nbformat_minor": 2,
+    "nbformat": 4,
+    "cells": [
+        {
+            "cell_type": "markdown",
+            "source": [
+                "1. <span style=\"font-weight: 600; color: rgb(36, 41, 47); font-family: -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Helvetica, Arial, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;; font-size: 16px;\">Készítsünk nézetet VSZOBA néven, amely megjeleníti a szobák adatai mellett a megfelelő szálláshely nevét, helyét és a csillagok számát is!</span>\n",
+                "\n",
+                "1. Az oszlopoknak nem szükséges külön nevet adni!\n",
+                "2. Teszteljük is a nézetet, pl: SELECT \\* FROM UJAENB\\_VSZOBA"
+            ],
+            "metadata": {
+                "azdata_cell_guid": "363cd270-6577-449b-9c89-d4b7c7972d52"
+            },
+            "attachments": {}
+        },
+        {
+            "cell_type": "code",
+            "source": [
+                ""
+            ],
+            "metadata": {
+                "azdata_cell_guid": "6ab9f633-a3b5-48c9-a1dc-4fb8235ae85b",
+                "language": "sql"
+            },
+            "outputs": [],
+            "execution_count": null
+        },
+        {
+            "cell_type": "markdown",
+            "source": [
+                "2. <span style=\"font-weight: 600; color: rgb(36, 41, 47); font-family: -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Helvetica, Arial, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;; font-size: 16px;\">Készítsen tárolt eljárást SPUgyfelFoglalasok, amely a paraméterként megkapott ügyfél azonosítóhoz tartozó foglalások adatait listázza!</span>\n",
+                "\n",
+                "1. Teszteljük a tárolt eljárás működését, pl: EXEC UJAENB\\_SPUgyfelFoglalasok 'laszlo2'"
+            ],
+            "metadata": {
+                "azdata_cell_guid": "ea75c103-911f-41a3-a32c-3d40fb3e4e4a"
+            },
+            "attachments": {}
+        },
+        {
+            "cell_type": "code",
+            "source": [
+                ""
+            ],
+            "metadata": {
+                "azdata_cell_guid": "222b9618-a8e8-42b4-a8e8-d002257122b7",
+                "language": "sql"
+            },
+            "outputs": [],
+            "execution_count": null
+        },
+        {
+            "cell_type": "markdown",
+            "source": [
+                "```\n",
+                "3. Készítsen skalár értékű függvényt UDFFerohely néven, amely visszaadja, hogy a paraméterként megkapott foglalás azonosítóhoz hány férőhelyes szoba tartozik!\n",
+                "a. Teszteljük a függvény működését!\n",
+                "```\n",
+                "\n",
+                "![](https://moodle.uni-corvinus.hu/theme/image.php/adaptable/atto_h5p/1709548862/icon)"
+            ],
+            "metadata": {
+                "azdata_cell_guid": "d7876bb9-76d0-4d07-baed-3cb17b2f96c3"
+            },
+            "attachments": {}
+        },
+        {
+            "cell_type": "code",
+            "source": [
+                ""
+            ],
+            "metadata": {
+                "azdata_cell_guid": "06d9a62b-9430-424b-a135-d80e675f0eb3",
+                "language": "sql"
+            },
+            "outputs": [],
+            "execution_count": null
+        }
+    ]
+}
